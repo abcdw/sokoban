a0b95d2f99883bef287513642066075a41af79f9
abcdw <andrewtropin@gmail.com>
2014-06-01 04:01:05 +0400

    Implemented logic

--- sokoban/Map.cs
+++ sokoban/Map.cs
@@ -18,8 +18,8 @@ namespace sokoban
 
         public enum FieldType {
             none,
-            wall,
             block,
+            wall,
             player,
         };
 
@@ -48,18 +48,41 @@ namespace sokoban
         {
             player = new Point();
             field = new int[height, width];
-            field[2, 2] = (int)FieldType.wall;
+            field[2, 2] = (int)FieldType.block;
             field[player.X, player.Y] = (int)FieldType.player;
         }
 
-        private bool isValidField(Point p)
+        private bool isInField(Point p)
         {
+            return p.X >= 0 && p.X < Width && p.Y >= 0 && p.Y < Height;
+        }
 
-            return true;
+        private bool isEmptyField(Point p)
+        {
+            return isInField(p) && field[p.X, p.Y] == (int)FieldType.none;
+        }
+
+        private bool isMoveableField(Point p)
+        {
+            return isInField(p) && field[p.X, p.Y] == (int)FieldType.block;
         }
 
         public void move(MoveType movement)
         {
+            Point newPos = player + (Size)dp[(int)movement];
+            Point blockAfter = newPos + (Size)dp[(int)movement];
+
+            if (isMoveableField(newPos) && !isEmptyField(blockAfter))
+                return;
+
+            if (!isEmptyField(newPos) && !isMoveableField(newPos))
+                return;
+
+
+            if (isMoveableField(newPos)) {
+                field[blockAfter.X, blockAfter.Y] = (int)FieldType.block;
+                field[newPos.X,     newPos.Y]     = (int)FieldType.none;
+            }
 
             field[player.X, player.Y] = (int)FieldType.none;
             player += (Size)dp[(int)movement];
